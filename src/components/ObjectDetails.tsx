67d062db5ef081bd753937154e55ca65afe6c832
amotoreq <antoni.ferenc@cyfroweszkoly.pl>
2026-06-04 14:37:29 +0200

    Implement object details panel

--- src/components/ObjectDetails.tsx
+++ src/components/ObjectDetails.tsx
@@ -1,1 +1,41 @@
+import type { ICelestialObject } from "../types/CelestialObject";
 
+interface IObjectDetailsProps {
+    selectedObject: ICelestialObject | null;
+}
+
+function ObjectDetails({
+    selectedObject,
+}:IObjectDetailsProps){
+    return (
+        <div>
+            {selectedObject ? (
+                <>
+                    <h2>{selectedObject.name}</h2>
+
+                    <img
+                        src={selectedObject.image}
+                        alt={selectedObject.name}
+                        width={400}
+                    />
+
+                    <p>
+                        Typ: {selectedObject.type}
+                    </p>
+
+                    <p>
+                        Odległość: {selectedObject.distance}
+                    </p>
+
+                    <p>
+                        {selectedObject.description}
+                    </p>
+                </>
+            ) : (
+                <h2>Wybierz obiekt do obserwacji</h2>
+            )}
+        </div>
+    );
+}
+
+export default ObjectDetails;
